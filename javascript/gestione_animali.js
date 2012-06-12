dc37471d1cdafb149a3c8ceefcdd6c5ec8554657
Pretto Antonio <prettoantonio@gmail.com>
2012-06-12 16:46:45 +0200

    no edit users ajax

--- javascript/gestione_animali.js
+++ javascript/gestione_animali.js
@@ -3,9 +3,6 @@ function handle_response(){
 }
 
 
-function edit(button){
-	location.href ="modifica_animale.cgi?name="+button.getAttribute("animal_name");
-}
 
 function destroy(button){
 	xmlhttp.open("GET","reply.cgi?watDo=animals&action=destroy&name="+button.getAttribute("animal_name"),true);
