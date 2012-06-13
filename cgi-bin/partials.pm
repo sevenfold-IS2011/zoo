91fa39c623062d9fc76d59427c7d5fded284e11d
palominoz <info@palomino.it>
2012-06-13 16:38:58 +0200

    w3c, non funzionante

--- cgi-bin/partials.pm
+++ cgi-bin/partials.pm
@@ -62,7 +62,10 @@ sub header{
 sub footer{
 	print '
 	<div id="footer">
-    <p>Monkey Island S.r.l. | P.I. 0349034129384 | Via le man dae simie 32 | Curtarolo (PD) | <a href="mappa.html">mappa del sito</a></p>
+    <p>Monkey Island S.r.l. | P.I. 0349034129384 | Via le man dae simie 32 | Curtarolo (PD) | <a href="mappa.html">mappa del sito</a> <br />
+	    <a href="http://validator.w3.org/check?uri=referer"><img
+	      src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" height="31" width="88" /></a>
+	  </p>
   </div>'
 }
 
