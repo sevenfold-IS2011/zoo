32e2bb774fe5595122c41b35fe056f1eb59d1acf
Pretto Antonio <prettoantonio@gmail.com>
2012-06-14 19:11:37 +0200

    fix

--- cgi-bin/Functions.pm
+++ cgi-bin/Functions.pm
@@ -321,7 +321,7 @@ sub get_user_age{
 }
 
 sub exhaustion_table{
-	$days = $_[0];
+	my $days = $_[0];
 	my $parser = XML::LibXML->new;
 	my $doc = $parser->parse_file("../xml/warehouse.xml");
 	my $root = $doc->getDocumentElement();
@@ -331,12 +331,12 @@ sub exhaustion_table{
 	my $foods = $xpc -> findnodes($xpath_exp, $doc);
 	my $foods_amount = $foods -> size();
 	my $food;
-	for(;$foods->size() > 0;){
-		$food = $foods -> pop();
-		if check_availability($food, $days){
-			
-		}
-	}
+#	for(;$foods->size() > 0;){
+#		$food = $foods -> pop();
+#		if check_availability($food, $days){
+#			
+#		}
+#	}
 }
 
 sub check_availability{
